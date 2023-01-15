1b83c615890f0ad76e20563d6996bfd8b7892aa9
davi_arttur <daviartur2004@gmail.com>
2023-01-15 12:00:02 -0300

    feat: add parametro UpdateProduct

--- src/application/useCase/UpdateProduct.ts
+++ src/application/useCase/UpdateProduct.ts
@@ -4,8 +4,8 @@ import { ProductRepository } from 'src/domain/repository';
 export class UpdateProduct {
   constructor(private productRepository: ProductRepository) {}
 
-  async update(dto: UpdateProductDto) {
-    const repo = await this.productRepository.updateOne(dto);
+  async update(dto: UpdateProductDto, productId: string) {
+    const repo = await this.productRepository.updateOne(dto, productId);
 
     return repo;
   }
