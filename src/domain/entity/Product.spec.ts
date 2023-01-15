a1897373914d472a868bbbb23f997cc63206275c
davi_arttur <daviartur2004@gmail.com>
2023-01-15 07:22:12 -0300

    test: criar mock para Product

--- src/domain/entity/Product.spec.ts
+++ src/domain/entity/Product.spec.ts
@@ -1,29 +1,35 @@
 import { ProductStatus } from '../interface';
 import { Product } from './Product';
 
+const ProductMock = {
+  id: 'testId',
+  category: 'electronic',
+  name: 'Iphone X',
+};
+
 describe('test Product entity', () => {
   it('should be possible create a new Product', () => {
     const product = new Product({
-      id: 'testId',
-      category: 'electronic',
-      name: 'Iphone X',
+      id: ProductMock.id,
+      category: ProductMock.category,
+      name: ProductMock.name,
       quantity: 10,
       status: ProductStatus.ACTIVE,
     });
 
     expect(product).toBeInstanceOf(Product);
-    expect(product.id).toBe('testId');
-    expect(product.category).toBe('electronic');
+    expect(product.id).toBe(ProductMock.id);
+    expect(product.category).toBe(ProductMock.category);
     expect(product.quantity).toBe(10);
-    expect(product.name).toBe('Iphone X');
+    expect(product.name).toBe(ProductMock.name);
     expect(product.status).toBe(ProductStatus.ACTIVE);
   });
 
   it('should create a product with status INACTIVE because quantity <= 0', () => {
     const product = new Product({
-      id: 'testId',
-      category: 'electronic',
-      name: 'Iphone X',
+      id: ProductMock.id,
+      category: ProductMock.category,
+      name: ProductMock.name,
       quantity: 0,
       status: ProductStatus.ACTIVE,
     });
@@ -33,9 +39,9 @@ describe('test Product entity', () => {
 
   it('should create a product with status INACTIVE with quantity >= 1', () => {
     const product = new Product({
-      id: 'testId',
-      category: 'electronic',
-      name: 'Iphone X',
+      id: ProductMock.id,
+      category: ProductMock.category,
+      name: ProductMock.name,
       quantity: 1,
       status: ProductStatus.INACTIVE,
     });
