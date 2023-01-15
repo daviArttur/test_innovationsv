3e62c3d2def9e175e56b70d5cf28c1076e117930
davi_arttur <daviartur2004@gmail.com>
2023-01-15 15:16:46 -0300

    validação UpdateProduct

--- src/application/useCase/UpdateProduct.spec.ts
+++ src/application/useCase/UpdateProduct.spec.ts
@@ -2,6 +2,7 @@ import { UpdateProductDto } from 'src/domain/dto/update-product.dto';
 import { Product } from 'src/domain/entity/Product';
 import { ProductStatus } from 'src/domain/interface';
 import { ProductRepositoryMock } from 'test/mock/repository';
+import { DuplicatedProductNameError } from '../errors/DuplicatedProductNameError';
 import { UpdateProduct } from './UpdateProduct';
 
 const dto: UpdateProductDto = {
@@ -15,7 +16,10 @@ describe('Test update product useCase', () => {
   it('should update an existent product', async () => {
     const repo = new ProductRepositoryMock();
     const repoSpy = jest.spyOn(repo, 'updateOne');
-    const updateResult = await new UpdateProduct(repo).update(dto);
+    const updateResult = await new UpdateProduct(repo).update(
+      dto,
+      'example id',
+    );
 
     expect(repoSpy).toHaveBeenCalledTimes(1);
     expect(updateResult).toBeInstanceOf(Product);
@@ -23,5 +27,20 @@ describe('Test update product useCase', () => {
     expect(updateResult.name).toBe(dto.name);
     expect(updateResult.quantity).toBe(dto.quantity);
     expect(updateResult.status).toBe(dto.status);
+  });
+
+  it('should error because nome is duplicated', async () => {
+    const repo = new ProductRepositoryMock();
+    const repoSpy = jest.spyOn(repo, 'findOne');
+
+    let error: Error;
+    try {
+      await new UpdateProduct(repo).update({ ...dto, name: '' }, 'example id');
+    } catch (err) {
+      error = err;
+    }
+
+    expect(repoSpy).toHaveBeenCalledTimes(1);
+    expect(error).toBeInstanceOf(DuplicatedProductNameError);
   });
 });
--- src/application/useCase/UpdateProduct.ts
+++ src/application/useCase/UpdateProduct.ts
@@ -1,10 +1,15 @@
 import { UpdateProductDto } from 'src/domain/dto/update-product.dto';
 import { ProductRepository } from 'src/domain/repository';
+import { DuplicatedProductNameError } from '../errors/DuplicatedProductNameError';
 
 export class UpdateProduct {
   constructor(private productRepository: ProductRepository) {}
 
   async update(dto: UpdateProductDto, productId: string) {
+    const validation = await this.productRepository.findOne(dto.name);
+
+    if (validation) throw new DuplicatedProductNameError();
+
     const repo = await this.productRepository.updateOne(dto, productId);
 
     return repo;
